53ca9bdc6dc3027774ff5014e1ceac53ac0e04a4
lincolodi <lincolodi095@gmail.com>
2024-11-14 19:02:18 +0530

    Create otp.js

---
+++ otp.js
@@ -0,0 +1,16 @@
+function sendOTP() {
+    const email = document.getElementById('email');
+    const otpverify = document.getElementById('otpverify')[0];
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
